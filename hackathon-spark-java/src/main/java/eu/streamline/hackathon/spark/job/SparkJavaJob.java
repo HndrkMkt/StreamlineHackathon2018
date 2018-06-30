05e0222f7a0ed02638ce8315ea859f1439019bec
Hendrik Makait <hendrik.makait@gmail.com>
2018-06-30 16:18:28 +0200

    Split up by bilateral relationsh

--- hackathon-spark-java/src/main/java/eu/streamline/hackathon/spark/job/SparkJavaJob.java
+++ hackathon-spark-java/src/main/java/eu/streamline/hackathon/spark/job/SparkJavaJob.java
@@ -5,22 +5,18 @@ import eu.streamline.hackathon.spark.scala.operations.GDELTInputReceiver;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.Optional;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.Function3;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Minutes;
 import org.apache.spark.streaming.State;
 import org.apache.spark.streaming.StateSpec;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
+import scala.Tuple3;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author behrouz
@@ -32,7 +28,7 @@ public class SparkJavaJob {
         ParameterTool params = ParameterTool.fromArgs(args);
         final String pathToGDELT = params.get("path");
         final Long duration = params.getLong("micro-batch-duration", 1000);
-        final String country = params.get("country", "USA");
+//        final String country = params.get("country", "USA");
 
         SparkConf conf = new SparkConf().setAppName("Spark Java GDELT Analyzer");
         String masterURL = conf.get("spark.master", "local[*]");
@@ -44,12 +40,12 @@ public class SparkJavaJob {
 
         // function to store intermediate values in the state
         // it is called in the mapWithState function of DStream
-        Function3<Date, Optional<Double>, State<Double>, Tuple2<Date, Double>> mappingFunc =
-                new Function3<Date, Optional<Double>, State<Double>, Tuple2<Date, Double>>() {
+        Function3<Tuple3<Date, String, String>, Optional<Double>, State<Double>, Tuple2<Tuple3<Date, String, String>, Double>> mappingFunc =
+                new Function3<Tuple3<Date, String, String>, Optional<Double>, State<Double>, Tuple2<Tuple3<Date, String, String>, Double>>() {
                     @Override
-                    public Tuple2<Date, Double> call(Date weekOfYear, Optional<Double> avgTone, State<Double> state) throws Exception {
+                    public Tuple2<Tuple3<Date, String, String>, Double> call(Tuple3<Date, String, String> keyTuple, Optional<Double> avgTone, State<Double> state) throws Exception {
                         Double sum = avgTone.orElse(0.0) + (state.exists() ? state.get() : 0.0);
-                        Tuple2<Date, Double> output = new Tuple2<>(weekOfYear, sum);
+                        Tuple2<Tuple3<Date, String, String>, Double> output = new Tuple2<>(keyTuple, sum);
                         state.update(sum);
                         return output;
                     }
@@ -57,19 +53,29 @@ public class SparkJavaJob {
 
         jssc
                 .receiverStream(new GDELTInputReceiver(pathToGDELT))
-                .filter(new Function<GDELTEvent, Boolean>() {
+//                .filter(new Function<GDELTEvent, Boolean>() {
+//                    @Override
+//                    public Boolean call(GDELTEvent gdeltEvent) throws Exception {
+//                        return Objects.equals(gdeltEvent.actor1Code_countryCode, country);
+//                    }
+//                })
+                .mapToPair(new PairFunction<GDELTEvent, Tuple3<Date, String, String>, Double>() {
                     @Override
-                    public Boolean call(GDELTEvent gdeltEvent) throws Exception {
-                        return Objects.equals(gdeltEvent.actor1Code_countryCode, country);
-                    }
-                })
-                .mapToPair(new PairFunction<GDELTEvent, Date, Double>() {
-                    @Override
-                    public Tuple2<Date, Double> call(GDELTEvent gdeltEvent) throws Exception {
+                    public Tuple2<Tuple3<Date, String, String>, Double> call(GDELTEvent gdeltEvent) throws Exception {
+                        String firstCountry = (gdeltEvent.actor1Code_countryCode != null ? gdeltEvent.actor1Code_countryCode : "UKNOWN");
+                        String secondCountry = (gdeltEvent.actor2Code_countryCode != null ? gdeltEvent.actor2Code_countryCode : "UKNOWN");
                         Calendar cal = Calendar.getInstance();
                         cal.setTime(gdeltEvent.dateAdded);
                         cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                        return new Tuple2<>(cal.getTime(), gdeltEvent.avgTone);
+                        if (firstCountry.compareTo(secondCountry) < 0) {
+                            return new Tuple2<>(
+                                    new Tuple3<>(cal.getTime(), firstCountry, secondCountry),
+                                    gdeltEvent.avgTone);
+                        } else {
+                            return new Tuple2<>(
+                                    new Tuple3<>(cal.getTime(), secondCountry, firstCountry),
+                                    gdeltEvent.avgTone);
+                        }
                     }
                 })
                 .reduceByKey(new Function2<Double, Double, Double>() {
@@ -79,11 +85,11 @@ public class SparkJavaJob {
                     }
                 })
                 .mapWithState(StateSpec.function(mappingFunc))
-                .map(new Function<Tuple2<Date, Double>, String>() {
+                .map(new Function<Tuple2<Tuple3<Date, String, String>, Double>, String>() {
                     @Override
-                    public String call(Tuple2<Date, Double> event) throws Exception {
+                    public String call(Tuple2<Tuple3<Date, String, String>, Double> event) throws Exception {
                         DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                        return "Country(" + country + "), Week(" + format.format(event._1()) + "), " +
+                        return "Country1(" + event._1()._2() + "), Country2(" + event._1()._3() + "), Week(" + format.format(event._1()._1()) + "), " +
                                 "AvgTone(" + event._2() + ")";
 
                     }
