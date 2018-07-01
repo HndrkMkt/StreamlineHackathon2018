6645a8ea59930d6636838bc1bff56f5873614a49
Hendrik Makait <hendrik.makait@gmail.com>
2018-07-01 13:58:45 +0200

    Add secondary bilateral data stream

--- hackathon-spark-java/src/main/java/eu/streamline/hackathon/spark/job/SparkJavaJob.java
+++ hackathon-spark-java/src/main/java/eu/streamline/hackathon/spark/job/SparkJavaJob.java
@@ -1,8 +1,5 @@
 package eu.streamline.hackathon.spark.job;
 
-import edu.stanford.nlp.ie.AbstractSequenceClassifier;
-import edu.stanford.nlp.ie.crf.CRFClassifier;
-import edu.stanford.nlp.ling.CoreLabel;
 import eu.streamline.hackathon.common.data.GDELTEvent;
 import eu.streamline.hackathon.spark.scala.operations.GDELTInputReceiver;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -12,13 +9,15 @@ import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.State;
 import org.apache.spark.streaming.StateSpec;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 import scala.Tuple3;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import scala.Tuple4;
 
 /**
@@ -27,30 +26,92 @@ import scala.Tuple4;
 public class SparkJavaJob {
 
     public static void main(String[] args) throws Exception {
-
-
         ParameterTool params = ParameterTool.fromArgs(args);
         final String pathToGDELT = params.get("path");
         final Long duration = params.getLong("micro-batch-duration", 1000);
-//        final String country = params.get("country", "USA");
 
         SparkConf conf = new SparkConf().setAppName("Spark Java GDELT Analyzer");
         String masterURL = conf.get("spark.master", "local[*]");
         conf.setMaster(masterURL);
         JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(duration));
 
-//        SparkContext ctx = SparkContext.getOrCreate(conf);
-//        ctx.addFile("/home/hendrik/projects/tu_berlin/dima/bdml/streamline-hackathon-boilerplate/english.all.3class.distsim.crf.ser.gz");
-//
-
-        String serializedClassifier = "english.all.3class.distsim.crf.ser.gz";
-
-        AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
-
-//        JavaStreamingContext jssc = new JavaStreamingContext(new JavaSparkContext(ctx), new Duration(duration));
         // checkpoint for storing the state
         jssc.checkpoint("checkpoint/");
 
+        JavaReceiverInputDStream<GDELTEvent> receiverStream = jssc
+                .receiverStream(new GDELTInputReceiver(pathToGDELT));
+
+        addActorTypeStream(receiverStream);
+
+        addBilateralToneStream(receiverStream);
+
+        jssc.start();
+        jssc.awaitTermination();
+
+    }
+
+    private static void addBilateralToneStream(JavaReceiverInputDStream<GDELTEvent> receiverStream) {
+        // function to store intermediate values in the state
+        // it is called in the mapWithState function of DStream
+        Function3<Tuple3<Date, String, String>, Optional<Tuple2<Integer, Double>>, State<Tuple2<Integer, Double>>, Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>>> mappingFunc =
+                new Function3<Tuple3<Date, String, String>, Optional<Tuple2<Integer, Double>>, State<Tuple2<Integer, Double>>, Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>>>() {
+                    @Override
+                    public Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>> call(Tuple3<Date, String, String> keyTuple, Optional<Tuple2<Integer, Double>> optValue, State<Tuple2<Integer, Double>> state) throws Exception {
+                        Tuple2<Integer, Double> value = optValue.orElse(new Tuple2<>(1, 0.));
+                        Tuple2<Integer, Double> stateValue = (state.exists() ? state.get() : new Tuple2<>(0, 0.));
+                        Tuple2<Integer, Double> resultState = new Tuple2<>(stateValue._1() + value._1(), stateValue._2() + value._2());
+                        Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>> output = new Tuple2<>(keyTuple, resultState);
+                        state.update(resultState);
+                        return output;
+                    }
+                };
+
+        receiverStream.flatMapToPair((new PairFlatMapFunction<GDELTEvent, Tuple3<Date, String, String>, Tuple2<Integer, Double>>() {
+            @Override
+            public Iterator<Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>>> call(GDELTEvent gdeltEvent) throws Exception {
+                ArrayList<Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>>> tuples = new ArrayList<>();
+                String firstCountry, secondCountry;
+                if (gdeltEvent.actor1Geo_countryCode != null) {
+                    firstCountry = gdeltEvent.actor1Geo_countryCode;
+                } else {
+                    return tuples.iterator();
+                }
+                if (gdeltEvent.actor2Geo_countryCode != null) {
+                    secondCountry = gdeltEvent.actor2Geo_countryCode;
+                } else {
+                    return tuples.iterator();
+                }
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(gdeltEvent.dateAdded);
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                tuples.add(new Tuple2<>(
+                        new Tuple3<>(cal.getTime(), firstCountry, secondCountry),
+                        new Tuple2<>(1, gdeltEvent.avgTone)));
+                tuples.add(new Tuple2<>(
+                        new Tuple3<>(cal.getTime(), secondCountry, firstCountry),
+                        new Tuple2<>(1, gdeltEvent.avgTone)));
+                return tuples.iterator();
+            }
+        }))
+                .reduceByKey(new Function2<Tuple2<Integer, Double>, Tuple2<Integer, Double>, Tuple2<Integer, Double>>() {
+                    @Override
+                    public Tuple2<Integer, Double> call(Tuple2<Integer, Double> one, Tuple2<Integer, Double> two) throws Exception {
+                        return new Tuple2<>(one._1() + two._1(), one._2() + two._2());
+                    }
+                })
+                .mapWithState(StateSpec.function(mappingFunc))
+                .map(new Function<Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>>, String>() {
+                    @Override
+                    public String call(Tuple2<Tuple3<Date, String, String>, Tuple2<Integer, Double>> event) throws Exception {
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                        return "" + event._1()._2() + ";" + event._1()._3() + ";" + format.format(event._1()._1()) + ";" +
+                                event._2()._2() / event._2()._1();
+                    }
+                })
+                .dstream().saveAsTextFiles("output/bilateral_relationships", "");
+    }
+
+    private static void addActorTypeStream(JavaReceiverInputDStream<GDELTEvent> receiverStream) {
 
         // function to store intermediate values in the state
         // it is called in the mapWithState function of DStreamclassifiers/
@@ -82,77 +143,46 @@ public class SparkJavaJob {
                     }
                 };
 
-//        Function<Iterable<Tuple2<String, Integer>, Iterable<Tuple2<String, Integer>> sortingFunct =
-//                new Function<Iterable<Tuple2<String, Integer>, Iterable<Tuple2<String, Integer>>() {
-//                    @Override
-//                    public Iterable<Tuple2<String, Integer>> call(Iterable<Tuple2<String, Integer>> input) throws Exception {
-//                        ArrayList<Tuple2<String, Integer>> tuples = new ArrayList<>();
-//                        for (Tuple2<String, Integer> tuple : input) {
-//                            tuples.add(tuple);
-//                        }
-//                        tuples.sort(new Comparator);
-//                        Integer sum = count.orElse(1) + (state.exists() ? state.get() : 0);
-//                        Tuple2<Tuple4<Date, String, String, String>, Integer> output = new Tuple2<>(keyTuple, sum);
-//                        state.update(sum);
-//                        return output;
-//                    }
-//                };
+        receiverStream.flatMapToPair(new PairFlatMapFunction<GDELTEvent, Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>() {
+            @Override
+            public Iterator<Tuple2<Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>> call(GDELTEvent gdeltEvent) {
+                ArrayList<Tuple2<Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>> tuples = new ArrayList<>();
 
-        jssc
-                .receiverStream(new GDELTInputReceiver(pathToGDELT))
-//                .filter(new Function<GDELTEvent, Boolean>() {
-//                    @Override
-//                    public Boolean call(GDELTEvent gdeltEvent) throws Exception {
-//                        return Objects.equals(gdeltEvent.actor1Code_countryCode, country);
-//                    }
-//                })
-//                .mapToPair(new PairFunction<GDELTEvent, Tuple3<Date, String, String>, Tuple2<String, String>>() {
-//                    @Override
-//                    public Tuple2<Tuple3<Date, String, String>, Tuple2<String, String>> call(GDELTEvent gdeltEvent) throws Exception {
-//                        String firstCountry = (gdeltEvent.actor1Code_countryCode != null ? gdeltEvent.actor1Code_countryCode : "UKNOWN");
-//                        String secondCountry = (gdeltEvent.actor2Code_countryCode != null ? gdeltEvent.actor2Code_countryCode : "UKNOWN");
-//
-//                        String firstActor = (gdeltEvent.actor1Code_name != null ? gdeltEvent.actor1Code_name : "UKNOWN");
-//                        String secondActor = (gdeltEvent.actor2Code_name != null ? gdeltEvent.actor2Code_name : "UKNOWN");
-//                        Calendar cal = Calendar.getInstance();
-//                        cal.setTime(gdeltEvent.dateAdded);
-//                        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-//                        if (firstCountry.compareTo(secondCountry) < 0) {
-//                            return new Tuple2<>(
-//                                    new Tuple3<>(cal.getTime(), firstCountry, secondCountry),
-//                                    new Tuple2<>(firstActor, secondActor));
-//                        } else {
-//                            return new Tuple2<>(
-//                                    new Tuple3<>(cal.getTime(), secondCountry, firstCountry),
-//                                    new Tuple2<>(secondActor, firstActor));
-//                        }
-//                    }
-//                })
-                .flatMapToPair(new PairFlatMapFunction<GDELTEvent, Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>() {
-                    @Override
-                    public Iterator<Tuple2<Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>> call(GDELTEvent gdeltEvent) {
-                        ArrayList<Tuple2<Tuple4<Date, String, String, String>, Tuple2<Integer, Double>>> tuples = new ArrayList<>();
+                String firstCountry = (gdeltEvent.actor1Geo_countryCode != null ? gdeltEvent.actor1Geo_countryCode : "UKNOWN");
+                String secondCountry = (gdeltEvent.actor2Geo_countryCode != null ? gdeltEvent.actor2Geo_countryCode : "UKNOWN");
 
-                        String firstCountry = (gdeltEvent.actor1Code_countryCode != null ? gdeltEvent.actor1Code_countryCode : "UKNOWN");
-                        String secondCountry = (gdeltEvent.actor2Code_countryCode != null ? gdeltEvent.actor2Code_countryCode : "UKNOWN");
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(gdeltEvent.dateAdded);
+                cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+                Tuple2<Integer, Double> value = new Tuple2<>(1, gdeltEvent.avgTone);
 
-                        String firstActor = (gdeltEvent.actor1Code_name != null ? gdeltEvent.actor1Code_name : "UKNOWN");
-                        String secondActor = (gdeltEvent.actor2Code_name != null ? gdeltEvent.actor2Code_name : "UKNOWN");
+                LinkedList<String> actorTypes = new LinkedList<>();
 
-                        Calendar cal = Calendar.getInstance();
-                        cal.setTime(gdeltEvent.dateAdded);
-                        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-                        Tuple2<Integer, Double> value = new Tuple2<>(1, gdeltEvent.avgTone);
-                        if (firstCountry.compareTo(secondCountry) < 0) {
-                            tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), firstCountry, secondCountry, firstActor), value));
-                            tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), firstCountry, secondCountry, secondActor), value));
-                        } else {
-                            tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), secondCountry, firstCountry, firstActor), value));
-                            tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), secondCountry, firstCountry, secondActor), value));
-                        }
-                        return tuples.iterator();
-                    }
-                }).reduceByKey(new Function2<Tuple2<Integer, Double>, Tuple2<Integer, Double>, Tuple2<Integer, Double>>() {
+                if (gdeltEvent.actor1Code_type1Code != null) {
+                    actorTypes.add(gdeltEvent.actor1Code_type1Code);
+                }
+                if (gdeltEvent.actor1Code_type2Code != null) {
+                    actorTypes.add(gdeltEvent.actor1Code_type2Code);
+                }
+                if (gdeltEvent.actor1Code_type3Code != null) {
+                    actorTypes.add(gdeltEvent.actor1Code_type3Code);
+                }
+                if (gdeltEvent.actor2Code_type1Code != null) {
+                    actorTypes.add(gdeltEvent.actor2Code_type1Code);
+                }
+                if (gdeltEvent.actor2Code_type2Code != null) {
+                    actorTypes.add(gdeltEvent.actor2Code_type2Code);
+                }
+                if (gdeltEvent.actor2Code_type3Code != null) {
+                    actorTypes.add(gdeltEvent.actor2Code_type3Code);
+                }
+                for (String actorType: actorTypes) {
+                    tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), firstCountry, secondCountry, actorType), value));
+                    tuples.add(new Tuple2<>(new Tuple4<>(cal.getTime(), secondCountry, firstCountry, actorType), value));
+                }
+                return tuples.iterator();
+            }
+        }).reduceByKey(new Function2<Tuple2<Integer, Double>, Tuple2<Integer, Double>, Tuple2<Integer, Double>>() {
             @Override
             public Tuple2<Integer, Double> call(Tuple2<Integer, Double> one, Tuple2<Integer, Double> two) throws Exception {
                 return new Tuple2<>(one._1() + two._1(), one._2() + two._2());
@@ -176,22 +206,14 @@ public class SparkJavaJob {
             }
         })
                 .mapValues(sortingFunc)
-
-//                .mapValues()
-//
-//                .map(new Function<Tuple2<Tuple4<Date, String, String>, Integer>, String>() {
-//                    @Override
-//                    public String call(Tuple2<Tuple4<Date, String, String, String>, Integer> event) throws Exception {
-//                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-//                        return "Country1(" + event._1()._2() + "), Country2(" + event._1()._3() + "), , Actor(" + event._1()._4() + "), Week(" + format.format(event._1()._1()) + "), " +
-//                                "Count(" + event._2() + ")";
-//
-//                    }
-//                })
-                .print();
-
-        jssc.start();
-        jssc.awaitTermination();
-
+                .map(new Function<Tuple2<Tuple3<Date, String, String>, List<Tuple3<String, Integer, Double>>>, String>() {
+                    @Override
+                    public String call(Tuple2<Tuple3<Date, String, String>, List<Tuple3<String, Integer, Double>>> event) throws Exception {
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                        return "" + event._1()._2() + ";" + event._1()._3() + ";" + format.format(event._1()._1()) + ";" +
+                                event._2().toString();
+                    }
+                })
+                .dstream().saveAsTextFiles("output/week", "");
     }
 }
